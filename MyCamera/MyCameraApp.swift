eb3de63fb2a8e8337c896e78355de5df408dbb1a
mochidaharuki <mochidaharuki@plise.co.jp>
2023-04-28 15:47:13 +0900

    Initial Commit

---
+++ MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by 持田晴生 on 2023/04/28.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
